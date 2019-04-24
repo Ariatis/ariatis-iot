f1a0fef42ed1d260491760f30ab974e8e952927b
Fabien Cordon <fcordon1@hotmail.fr>
2019-04-24 13:39:31 +0200

    Drag & drop done

--- client/src/components/GererEnsembles.js
+++ client/src/components/GererEnsembles.js
@@ -1,6 +1,8 @@
 import React, { Component } from 'react'
 import { Container, Row, Col, Button, Form, Card } from 'react-bootstrap'
-// import axios from 'axios'
+import axios from 'axios'
+
+import capteurImg from '../img/capteurs.png'
 
 class GererEnsembles extends Component {
   constructor(props) {
@@ -13,28 +15,73 @@ class GererEnsembles extends Component {
     }
   }
 
-  // componentDidMount() {
-  //   this.callCapteurs()
-  //     .then(res => this.setState({ data: res }))
-  //     .catch(err => console.log(err))
-  // }
-  //
-  // callCapteurs = async () => {
-  //   const response = await axios.get('/capteurs')
-  //   const body = await response.data
-  //
-  //   if (response.status !== 200) {
-  //     throw Error(body.message)
-  //   }
-  //   return body
-  // }
+  componentDidMount() {
+    this.callCapteurs()
+      .then(res => this.setState({ data: res }))
+      .catch(err => console.log(err))
+  }
 
-  addGroup = () => {
-    // const group = (
-    //
-    // )
-    //
-    // return group
+  callCapteurs = async () => {
+    const response = await axios.get('/capteurs')
+    const body = await response.data
+
+    if (response.status !== 200) {
+      throw Error(body.message)
+    }
+    return body
+  }
+
+  addGroup(e) {
+    e.preventDefault()
+    let newArray = [...this.state.group]
+    let nomEnsemble = document.getElementById('nomEnsemble').value
+
+    newArray.push({nom:nomEnsemble})
+
+    this.setState({
+      group: newArray
+    })
+  }
+
+  dragging(id) {
+    let element = document.getElementById(id)
+    element.style.cursor = 'grabbing'
+  }
+
+  draggable(id) {
+    let element = document.getElementById(id)
+    element.style.cursor = 'grab'
+  }
+
+  onDragStart = (e, id, nom) => {
+    e.dataTransfer.setData('id', id)
+    e.dataTransfer.setData('nom', nom)
+  }
+
+  onDragOver = (e) => {
+    e.preventDefault()
+  }
+
+  onDrop = (e, groupName) => {
+    let capteurID = e.dataTransfer.getData("id")
+    let capteurNom =  e.dataTransfer.getData("nom")
+
+    let groups = this.state.group.filter(group => {
+      if (group.nom === groupName) {
+        if (group.capteurs === undefined) {
+          group.capteurs = [{capteurID: capteurID, capteurNom: capteurNom}]
+        } else {
+          let newCapteur = [...group.capteurs]
+          newCapteur.push({capteurID: capteurID, capteurNom: capteurNom})
+          group.capteurs = newCapteur
+        }
+      }
+      return groups
+    })
+
+    this.setState({
+      ...this.state
+    })
   }
 
   render() {
@@ -44,15 +91,36 @@ class GererEnsembles extends Component {
           <Form onSubmit={this.addGroup.bind(this)}>
             <Form.Row>
               <Col>
-                <Form.Control type="text" placeholder="Nom de l'ensemble" />
+                <Form.Control id="nomEnsemble" type="text" placeholder="Nom de l'ensemble" />
               </Col>
               <Col>
-                <Button>Ajouter un ensemble</Button>
+                <Button type="submit">Ajouter un ensemble</Button>
               </Col>
             </Form.Row>
           </Form>
           <Row>
-            <Col xs={12}>
+            <Col xs={12} md={6}>
+              <Card>
+                <Card.Header>
+                  <Card.Title>
+                    Mes capteurs
+                  </Card.Title>
+                </Card.Header>
+                <Card.Body>
+                    {
+                      this.state.data !== null && this.state.data.map(capteur => {
+                        return(
+                          <Col xs={12} md={6} draggable key={capteur._id} id={capteur._id} onDragStart={(e) => this.onDragStart(e, capteur._id, capteur.nom)} onMouseDown={this.dragging.bind(this, capteur._id)} onMouseUp={this.draggable.bind(this, capteur._id)}>
+                            <img src={capteurImg} alt={capteur.nom} />
+                            {capteur.nom}
+                          </Col>
+                        )
+                      })
+                    }
+                </Card.Body>
+              </Card>
+            </Col>
+            <Col xs={12} md={6}>
               {
                 this.state.group.map((group, i) => {
                   return (
@@ -62,8 +130,17 @@ class GererEnsembles extends Component {
                           {group.nom}
                         </Card.Title>
                       </Card.Header>
-                      <Card.Body>
-
+                      <Card.Body onDragOver={(e)=>this.onDragOver(e)} onDrop={(e)=>this.onDrop(e, group.nom)}>
+                        {
+                          group.capteurs !== undefined && group.capteurs.map((capteur, i) => {
+                            return(
+                              <Col xs={12} md={6} key={i}>
+                                <img src={capteurImg} alt={capteur.capteurNom} />
+                                {capteur.capteurNom}
+                              </Col>
+                            )
+                          })
+                        }
                       </Card.Body>
                     </Card>
                   )
