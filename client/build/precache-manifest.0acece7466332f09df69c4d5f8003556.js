b7602029ea673fd8a045e976de475ad0d42c61f5
Fabien Cordon <fcordon1@hotmail.fr>
2019-11-19 14:32:49 +0100

    release v12

---
+++ client/build/precache-manifest.0acece7466332f09df69c4d5f8003556.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dcbc8a13684bfc6c189751391aa53a9e",
+    "url": "/index.html"
+  },
+  {
+    "revision": "5f0b212c936a7e13fb7a",
+    "url": "/static/css/main.de8f7c5e.chunk.css"
+  },
+  {
+    "revision": "b25816d6c97182a09cb7",
+    "url": "/static/js/2.f44f1807.chunk.js"
+  },
+  {
+    "revision": "5f0b212c936a7e13fb7a",
+    "url": "/static/js/main.925e2913.chunk.js"
+  },
+  {
+    "revision": "f4673b89ab4d04c82ae9",
+    "url": "/static/js/runtime-main.03f39fd1.js"
+  },
+  {
+    "revision": "3163df4569a5ac688baab0ac02328700",
+    "url": "/static/media/ariatis_logo_full.3163df45.png"
+  }
+]);
